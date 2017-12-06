a719b4498e768baf3dd87e8985128c339c43d8ea
eggplant60 <nagoyan240@gmail.com>
2017-12-06 21:10:39 +0900

    changing layout

--- plotly/plot.py
+++ plotly/plot.py
@@ -11,29 +11,32 @@ import plotly.graph_objs as go
 # ---------------------------------------
 # Reading data and graph layout
 # ---------------------------------------
-layout1 = go.Layout(
-    legend={"x":0.8, "y":0.2},
-    xaxis={"title":"Date"},
-    yaxis={"title":"Humidity"},
-    yaxis2={"title":"Tempareture", "overlaying":"y", "side":"right"},
-)
-layout2 = go.Layout(
-    legend={"x":0.8, "y":0.1},
-    xaxis={"title":"Date"},
-    yaxis={"title":"Pressure"}
-)
-layout3 = go.Layout(
-    legend={"x":0.8, "y":0.1},
-    xaxis={"title":"Date"},
-    yaxis={"title":"Luminous"}
-)
 
+# def layout_templete2(title, y_axis1, y_axis2):
+#     return go.Layout(
+#         title=title,
+#         xaxis={"title":"Date"},
+#         yaxis={"title":y_axis1},
+#         yaxis2={"title":"y_", "overlaying":"y", "side":"right"},
+#     )
 def serve_layout():
-    raw = pd.read_csv("../log.csv", names=('date', 'hum', 'temp', 'pres', 'lux'))
+    
+    def layout_template(title, y_axis):
+        return go.Layout(
+            title=title,
+            legend={"x": 0.1, "y": 0.8},
+            xaxis={"title":"Date"},
+            yaxis={"title":y_axis},
+            width=700,
+            height=500,
+        )
+
+    raw = pd.read_csv("/home/naoya/airalarm/log.csv",
+                      names=('date', 'hum', 'temp', 'pres', 'lux'))
 
     temp_hum = [
-        go.Scatter(x=raw["date"], y=raw["temp"], name="Temparature", mode = 'lines+markers'),
-        go.Scatter(x=raw["date"], y=raw["hum"], name="Humidity", yaxis="y2", mode = 'lines+markers'),
+        go.Scatter(x=raw["date"], y=raw["temp"], name="Temparature[C]", mode = 'lines+markers'),
+        go.Scatter(x=raw["date"], y=raw["hum"], name="Humidity[%]",  mode = 'lines+markers'),
     ]
     d_pres = [go.Scatter(x=raw["date"], y=raw["pres"], name="Pressure", mode = 'lines+markers')]
     d_lux = [go.Scatter(x=raw["date"], y=raw["lux"], name="Luminous", mode = 'lines+markers')]
@@ -50,23 +53,25 @@ def serve_layout():
             value='v_off'
         ),
 
-        html.H3(children='Temparature and Humidity'),
+        html.H3(children='Graphs'),
         dcc.Graph(
             id='graph-temp-hum',
-            figure={'data': temp_hum, 'layout': layout1}
-        ),
-
-        html.H3(children='Pressure'),
+            figure={
+                'data': temp_hum,
+                'layout': layout_template("Temparature and Humidity", "Value")
+            }),
         dcc.Graph(
             id='graph-pres',
-            figure={'data': d_pres, 'layout': layout2}
-        ),
-
-        html.H3(children='Luminous'),
+            figure={
+                'data': d_pres,
+                'layout': layout_template("Pressure", "Pressure[hPa]")
+            }),
         dcc.Graph(
             id='graph-lux',
-            figure={'data': d_lux, 'layout': layout3}
-        )
+            figure={
+                'data': d_lux,
+                'layout': layout_template("Luminous intensity", "Luminous intensity[lux]")
+            })
     ])
 
 
