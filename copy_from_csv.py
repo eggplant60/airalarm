2930e82e6c06f760d199b03389aca46647f0f528
eggplant60 <nagoyan240@gmail.com>
2019-01-24 20:55:59 +0900

    [add] CSV からログ取り込み

---
+++ copy_from_csv.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+import psycopg2
+from postgres import * # user, password
+
+
+def get_connection():
+    return psycopg2.connect(
+        host = "192.168.11.205",
+        port = 5432,
+        database = "airalarm",
+        user = user,
+        password = password)
+
+
+def process_file(con, cur, table_name, file_object):
+    cur.copy_expert(
+        sql="COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','" % table_name,
+        file=file_object
+    )
+    con.commit() # auto commitではないので、明示的なcommitの実行が必要
+
+
+
+table_name = "environment"
+csv_path   = "log.csv"
+
+with get_connection() as con:
+    with con.cursor() as cur:
+        cur.execute("""CREATE TABLE IF NOT EXISTS %s (
+                         date timestamp PRIMARY KEY, 
+                         humidity float,
+                         temperature float,
+                         pressure float,
+                         illuminanceinteger float
+        );""" % table_name)
+
+        cur.execute("DELETE FROM %s;" % table_name)
+
+        with open(csv_path, "r") as f:
+            process_file(con, cur, table_name, f)
+
+        cur.execute("SELECT COUNT(*) FROM %s;" % table_name)
+        results = cur.fetchall()
+        print(results)
+
+#cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (200, "ghi_jkl"))
+
