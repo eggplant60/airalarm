947586e72a1b20085726de727334ea4d4d834c3d
eggplant60 <nagoyan240@gmail.com>
2019-01-24 22:07:31 +0900

    [fix] モジュールとして使うために if __name__ 以下を追加

--- copy_from_csv.py
+++ copy_from_csv.py
@@ -20,28 +20,28 @@ def process_file(con, cur, table_name, file_object):
     con.commit() # auto commitではないので、明示的なcommitの実行が必要
 
 
+if __name__ == '__main__':
+    table_name = "environment"
+    csv_path   = "log.csv"
 
-table_name = "environment"
-csv_path   = "log.csv"
+    with get_connection() as con:
+        with con.cursor() as cur:
+            cur.execute("DROP TABLE %s;" % table_name)
+            cur.execute("""CREATE TABLE IF NOT EXISTS %s (
+                             date timestamp PRIMARY KEY, 
+                             humidity float,
+                             temperature float,
+                             pressure float,
+                             illuminance float
+            );""" % table_name)
 
-with get_connection() as con:
-    with con.cursor() as cur:
-        cur.execute("""CREATE TABLE IF NOT EXISTS %s (
-                         date timestamp PRIMARY KEY, 
-                         humidity float,
-                         temperature float,
-                         pressure float,
-                         illuminanceinteger float
-        );""" % table_name)
+            # cur.execute("DELETE FROM %s;" % table_name)
 
-        cur.execute("DELETE FROM %s;" % table_name)
+            with open(csv_path, "r") as f:
+                process_file(con, cur, table_name, f)
 
-        with open(csv_path, "r") as f:
-            process_file(con, cur, table_name, f)
-
-        cur.execute("SELECT COUNT(*) FROM %s;" % table_name)
-        results = cur.fetchall()
-        print(results)
+            cur.execute("SELECT COUNT(*) FROM %s;" % table_name)
+            results = cur.fetchall()
+            print(results)
 
 #cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (200, "ghi_jkl"))
-
