3e5e39847bfb361c40607d73b41c6b2e48f0e2de
eggplant60 <nagoyan240@gmail.com>
2019-02-05 23:13:08 +0900

    [update] 追加されたログのみをDBにINSERTできるように修正

--- copy_from_csv.py
+++ copy_from_csv.py
@@ -11,12 +11,18 @@ def get_connection():
         user = user,
         password = password)
 
-
+# Primary key の重複は無視してコピー(UPSERT)
 def process_file(con, cur, table_name, file_object):
-    cur.copy_expert(
-        sql="COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','" % table_name,
-        file=file_object
-    )
+    cur.execute(sql_create % "tmp")
+    sql_copy = "COPY tmp FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+    cur.copy_expert(sql=sql_copy, file=file_object)
+    sql_upsert = """INSERT INTO %s (date, humidity, temperature, pressure, illuminance)
+                        SELECT date, humidity, temperature, pressure, illuminance
+                        FROM tmp
+                    ON CONFLICT ON CONSTRAINT environment_pkey
+                    DO NOTHING;""" % table_name  # environment_pkey は制約名
+    cur.execute(sql_upsert)
+    cur.execute("DROP TABLE %s;" % "tmp")  # 削除
     con.commit() # auto commitではないので、明示的なcommitの実行が必要
 
 
@@ -24,17 +30,20 @@ if __name__ == '__main__':
     table_name = "environment"
     csv_path   = "log.csv"
 
+    sql_create = """CREATE TABLE IF NOT EXISTS %s (
+                           date timestamp PRIMARY KEY,
+                           humidity float,
+                           temperature float,
+                           pressure float,
+                           illuminance float);
+    """
     with get_connection() as con:
         with con.cursor() as cur:
-            cur.execute("DROP TABLE %s;" % table_name)
-            cur.execute("""CREATE TABLE IF NOT EXISTS %s (
-                             date timestamp PRIMARY KEY, 
-                             humidity float,
-                             temperature float,
-                             pressure float,
-                             illuminance float
-            );""" % table_name)
+            # コメントインしない
+            #cur.execute("DROP TABLE %s;" % table_name)
+            cur.execute(sql_create % table_name)
 
+            # むやみにコメントインしない
             # cur.execute("DELETE FROM %s;" % table_name)
 
             with open(csv_path, "r") as f:
